3e7a16f2a47f650d0558e8f415cf1cbe11b12970
Chanjong Baek <readandaction@gmail.com>
2020-08-03 15:44:56 +0900

    Install middwares

--- app.js
+++ app.js
@@ -1,4 +1,18 @@
 import express from "express";
+import morgan from "morgan";
+import helmet from "helmet";
+import cookieParser from "cookie-parser";
+import bodyParser from "body-parser";
+
 const app = express();
+app.use(helmet());
+app.use(morgan("dev"));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(cookieParser());
+
+app.get("/", (req, res) => {
+  res.send("test");
+});
 
 export default app;
